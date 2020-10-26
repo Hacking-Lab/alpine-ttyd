650f0bc8918e32f6d72f8a430f55a9ee2657358e
Ivan Buetler <ivan.buetler@compass-security.com>
2020-10-26 11:45:12 +0100

    init release 3.2

---
+++ build.sh
@@ -0,0 +1,8 @@
+
+rm -rf ./docker-alpine-libwebsockets-with-libuv
+git clone https://github.com/matti/docker-alpine-libwebsockets-with-libuv.git
+
+rm -rf ./app
+mv ./docker-alpine-libwebsockets-with-libuv/app .
+
+docker build --no-cache -t hackinglab/alpine-ttyd:3.2.0 -t hackinglab/alpine-ttyd:3.2 -t hackinglab/alpine-ttyd:latest -f Dockerfile .
