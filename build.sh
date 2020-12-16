bec3ac532eff3ed1efaae7561383c72acbbb1d0f
Ivan Buetler <ivan.buetler@compass-security.com>
2020-12-16 11:40:43 +0100

    update

--- build.sh
+++ build.sh
@@ -6,3 +6,4 @@ rm -rf ./app
 mv ./docker-alpine-libwebsockets-with-libuv/app .
 
 docker build --no-cache -t hackinglab/alpine-ttyd:3.2.0 -t hackinglab/alpine-ttyd:3.2 -t hackinglab/alpine-ttyd:latest -f Dockerfile .
+
