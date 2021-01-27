2a95dd069ff98e4aed41a7f704cbc8ebcbd8c48b
Ivan Buetler <ivan.buetler@compass-security.com>
2021-01-27 11:43:56 +0100

    update

--- build-release.sh
+++ build-release.sh
@@ -6,6 +6,10 @@ git clone https://github.com/matti/docker-alpine-libwebsockets-with-libuv.git
 rm -rf ./app
 mv ./docker-alpine-libwebsockets-with-libuv/app .
 
-docker build --no-cache -t hackinglab/alpine-ttyd:3.2.0 -t hackinglab/alpine-ttyd:3.2 -t hackinglab/alpine-ttyd:latest -f Dockerfile .
+docker build --no-cache -t hackinglab/alpine-ttyd:$1.0 -t hackinglab/alpine-ttyd:$1 -t hackinglab/alpine-ttyd:latest -f Dockerfile .
+
+docker push hackinglab/alpine-ttyd
+docker push hackinglab/alpine-ttyd:$1
+docker push hackinglab/alpine-ttyd:$1.0
 
 rm -rf ./docker-alpine-libwebsockets-with-libuv
