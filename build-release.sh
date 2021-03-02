8e135be37ab85ce5d0cb21a0d86cfb7eafd8ba72
Ivan Buetler <ivan.buetler@compass-security.com>
2021-03-02 11:57:51 +0100

    fixing docker

--- build-release.sh
+++ build-release.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-rm -rf ./docker-alpine-libwebsockets-with-libuv
-git clone https://github.com/matti/docker-alpine-libwebsockets-with-libuv.git
-
-rm -rf ./app
-mv ./docker-alpine-libwebsockets-with-libuv/app .
-
 docker build --no-cache -t hackinglab/alpine-ttyd:$1.0 -t hackinglab/alpine-ttyd:$1 -t hackinglab/alpine-ttyd:latest -f Dockerfile .
 
 docker push hackinglab/alpine-ttyd
 docker push hackinglab/alpine-ttyd:$1
 docker push hackinglab/alpine-ttyd:$1.0
 
-rm -rf ./docker-alpine-libwebsockets-with-libuv
--- build.sh
+++ build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-rm -rf ./docker-alpine-libwebsockets-with-libuv
-git clone https://github.com/matti/docker-alpine-libwebsockets-with-libuv.git
-
-rm -rf ./app
-mv ./docker-alpine-libwebsockets-with-libuv/app .
-
 docker build --no-cache -t hackinglab/alpine-ttyd:3.2.0 -t hackinglab/alpine-ttyd:3.2 -t hackinglab/alpine-ttyd:latest -f Dockerfile .
 
-rm -rf ./docker-alpine-libwebsockets-with-libuv
